77cd21726a505e1e6ca254277bc8e1ac66853451
Svitlana Kozina <imssun@yandex.ru>
2016-03-19 18:57:15 +0200

    dz js1-2

--- js1-2/2/js/script.js
+++ js1-2/2/js/script.js
@@ -7,25 +7,35 @@ var n = 5;
 for(var i = 0; i < n; i++){
     names[i] = prompt('Введите имя пользователя', '');
     };
-
+alert(names);
 console.log(names);
 
 //Проверяем есть ли имя пользователя в массиве, приветствуем или сообщаем об ошибке
 
 function enterTo() {
+
+var result;
+result='startRes';
 var userName;
 userName = prompt('Уважаемый пользователь, введите Ваше имя:', '');
 
-for(i = 0; i<names.length; i++) {
+for(i = 0; i < names.length; i++) {
   if((userName === null) || (userName === '')) {
-    alert('К сожалению, все ушли на обед, приходите завтра.')
-    return;}
-  else if (userName === names[i]) {
+    alert('К сожалению, все ушли на обед, приходите завтра.');
+    result = 'unknUser';
+    console.log(result);
+    return result;} else if(userName == names[i]) {
     alert(userName + ', вот это да, Вы успешно вошли!:)');
-    return;}
-  else {
-    alert('К сожалению, все ушли на обед, приходите завтра.')
-    return;}}    
+    result = 'ourUser';
+    console.log(result);
+    return result;} else {
+    result = 'otherUser';
+    console.log(result);
+    }};
+if(result = 'otherUser'){
+    alert('Имя не найдено, зарегистрируйтесь.');
+    return result;
+}
 };
 
 enterTo();
